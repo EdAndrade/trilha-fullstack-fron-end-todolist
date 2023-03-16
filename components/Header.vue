13408219a7fafb5f3001842967f750d6abc43d42
edAndrade <edmilisonwork@gmail.com>
2023-03-16 22:33:12 +0100

    scaffold

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+	<section id="main-header">
+		<div>
+			<p>Logo</p>
+		</div>
+
+		<nav>
+			<ul>
+				<li>Add Todo</li>
+				<li>Edit Todo</li>
+			</ul>
+		</nav>
+	</section>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+	#main-header{
+		width: 100%;
+		height: 50px;
+		background-color: #222;
+		color: #fff;
+	}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<Header/>
+		<h1>Hello {{ color }}</h1>
+		<button @click="changeColorName()" >Change color</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "HomePage",
+
+		data(){
+			return {
+				color: 'red'
+			}
+		},
+
+		methods: {
+
+			changeColorName(){
+				this.color = 'blue'
+			}
+		}
+	};
+</script>
+
+<style scoped>
+</style>
